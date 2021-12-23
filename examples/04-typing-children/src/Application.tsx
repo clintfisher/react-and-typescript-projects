3acf17216473373d07ff68dd643f702923d08211
Clint Fisher <clintfisher.is@gmail.com>
2021-12-23 15:09:14 -0800

    lodash.shuffle needs types

--- examples/04-typing-children/src/Application.tsx
+++ examples/04-typing-children/src/Application.tsx
@@ -8,16 +8,40 @@
  * React.ReactChild[];
  */
 
- type BoxProps = { children: any };
+// ReactNode satisfies all the conditions
+// NOTE: check squiggly lines here from typescript
+// as well as the rendered view which shows the errors
+// a string or an html element, or a component
+// which will evenutally be an html element, etc
+
+import * as React from 'react';
+
+ type BoxProps = {
+   children: React.ReactNode
+};
 
  const Box = ({ children }: BoxProps) => {
+  console.log('children: ', children);
+   console.log('typeof(children): ', typeof(children));
+
+   if (Array.isArray(children)) {
+     console.log('children are arrays');
+   }
+
+   if (!Array.isArray(children) && typeof children === 'object') {
+    console.log('my children are an object but not arrays');
+  }
+
+  // console.log('children: ', children);
+  //  console.log('children.props: ', children.props);
+  //  console.log('children.props.type: ', children.props.type);
    return (
      <section style={{ padding: "1em", border: "5px solid purple" }}>
        {children}
      </section>
    );
  };
- 
+
  export default function Application() {
    return (
      <Box>
@@ -33,4 +57,3 @@
      </Box>
    );
  }
- 
