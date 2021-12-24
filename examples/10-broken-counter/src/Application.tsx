f28abc0b770af845c319b074fa9430f82dc28a55
Clint Fisher <clintfisher.is@gmail.com>
2021-12-24 10:15:29 -0800

    this one is tricky, wrote in comments what he said about returning a number to useEffect but its the lack of curly braces that made this error because of the implicit return

--- examples/10-broken-counter/src/Application.tsx
+++ examples/10-broken-counter/src/Application.tsx
@@ -2,8 +2,20 @@ import { useEffect, useState } from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
-
-  useEffect(() => setTimeout(() => setCount(count + 1), 1000), [count]);
+  // fix is add curly braces
+  /*
+    useEffect is expecting a function signature that returns
+    undefined. setTimeout returns a number.
+    because we are using an implicit return here w/out braces
+    we accidentally passed number to useEffect. typescripts built
+    in react types from the typescript package caught this error.
+    curly braces protects us from passing in code we didnt intend
+  */
+  useEffect(
+    () => {
+      setTimeout(() => setCount(count + 1), 1000)
+    }, [count]
+  );
 
   return <main className="count">{count}</main>;
 };
