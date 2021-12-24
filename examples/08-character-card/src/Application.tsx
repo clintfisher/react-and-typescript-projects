dee2c373aa6ba8011c729886ba47992e75571f55
Clint Fisher <clintfisher.is@gmail.com>
2021-12-24 09:20:35 -0800

    setting state

--- examples/08-character-card/src/Application.tsx
+++ examples/08-character-card/src/Application.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState, useEffect} from 'react';
 
 import { CharacterType, fetchCharacter } from './characters';
 
@@ -6,7 +6,20 @@ import { Loading } from './Loading';
 import { CharacterInformation } from './CharacterInformation';
 
 const Application = () => {
-  return <main></main>;
+  const [character, setCharacter] = useState<CharacterType | null>(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      fetchCharacter()
+      .then(c => { setCharacter(c); setLoading(false) });
+    }, 1000);
+  }, []);
+  return (
+    <main>
+      { loading && <Loading /> }
+      { character && <CharacterInformation character={character} /> }
+    </main>
+  );
 };
 
 export default Application;
