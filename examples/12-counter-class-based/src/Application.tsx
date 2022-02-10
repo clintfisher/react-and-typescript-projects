313fcd5ebbeb946913f29bbee203cd95776be80c
Clint Fisher <clintfisher.is@gmail.com>
2022-02-09 19:14:30 -0800

    updates

--- examples/12-counter-class-based/src/Application.tsx
+++ examples/12-counter-class-based/src/Application.tsx
@@ -1,20 +1,73 @@
-import { Component } from 'react';
+import { ChangeEvent, Component } from 'react';
 
-class Counter extends Component {
+type CounterProps = {
+  incident: string;
+}
+
+type CounterState = {
+  count: number;
+}
+
+class Counter extends Component<CounterProps, CounterState> {
+  /* he defines CounterState twice
+    it was for older version type hinting, its not needed
+    you can use use state = {}; but he likes keeping it in
+    personal preference
+  */
+  state: CounterState = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState(({ count }) => ({
+      count: count + 1
+    }));
+  }
+
+  decrement = () => {
+    this.setState(({ count }) => ({
+      count: count - 1
+    }));
+  }
+
+  reset = () => {
+    this.setState({count: 0});
+  }
+
+  // just passing in (event) as the param
+  // typescript  gets an implicit 'any' it only
+  // knows you just made a function on a class property
+  // so we have to tell it more about the event that we
+  // will have on the form submit
+  changeCount = (event: ChangeEvent<HTMLInputElement>) => {
+    /* the value in the dom from the input is a string
+    we said on line 8 its a number, so we need to coerce
+    here which we can do by adding the + sign before
+    event.target.value */
+    this.setState({count: +event.target.value});
+  }
+
   render() {
+    const { incident } = this.props;
+    const { count } = this.state;
     return (
       <main className="Counter">
-        <h1>Days Since Last Incident</h1>
-        <p className="count">0</p>
+        <h1>Days Since Last {incident}</h1>
+        <p className="count">{count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Reset</button>
-          <button>Decrement</button>
+          <button onClick={this.increment}>Increment</button>
+          <button onClick={this.reset}>Reset</button>
+          <button onClick={this.decrement}>Decrement</button>
         </section>
         <section className="controls">
           <form onSubmit={() => {}}>
             <label htmlFor="set-to">Set Count</label>
-            <input id="set-to" type="number" />
+            <input
+              id="set-to"
+              type="number"
+              value={count}
+              onChange={this.changeCount}
+            />
             <input type="submit" />
           </form>
         </section>
@@ -25,7 +78,7 @@ class Counter extends Component {
 
 class Application extends Component {
   render() {
-    return <Counter />;
+    return <Counter incident={'coffee spill'}/>;
   }
 }
 
